612b82425bc6b7a517a0d426ea58a9d966ca7137
6391-Bearbotics <uarobotics@ursulinestl.org>
2025-09-20 11:42:27 -0500

    PORTS

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -12,14 +12,14 @@ public class Ports {
     /* SUBSYSTEM CAN DEVICE IDS */
 
     public static final Integer CLAW_DIGITAL = 0;
-    public static final CanDeviceId CLAW_ROLLER = new CanDeviceId(61, CANBus.systemCore(0));
+    public static final CanDeviceId CLAW_ROLLER = new CanDeviceId(61, CANBus.systemCore(1));
 
-    public static final CanDeviceId ARM_MAIN = new CanDeviceId(60, CANBus.systemCore(0));
+    public static final CanDeviceId ARM_MAIN = new CanDeviceId(60, CANBus.systemCore(1));
 
     public static final Integer CLIMBER_DIGITAL = 2;
-    public static final CanDeviceId ELEVATOR_MAIN = new CanDeviceId(51, CANBus.systemCore(0));
-    public static final CanDeviceId ELEVATOR_FOLLOWER = new CanDeviceId(50, CANBus.systemCore(0));
+    public static final CanDeviceId ELEVATOR_MAIN = new CanDeviceId(51, CANBus.systemCore(1));
+    public static final CanDeviceId ELEVATOR_FOLLOWER = new CanDeviceId(50, CANBus.systemCore(1));
 
-    public static final CanDeviceId CLIMBER = new CanDeviceId(8, CANBus.systemCore(0));
+    public static final CanDeviceId CLIMBER = new CanDeviceId(8, CANBus.systemCore(1));
 
 }
